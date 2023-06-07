dc9226bcb5799957536a853ca8ffa0c0b5b48934
Ajraa <salajkapavel@gmail.com>
2023-06-07 14:22:01 +0200

    Exercise 3 and 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    ret_arr = [];
+    array.forEach(element => {
+        if (!items.includes(element))
+            ret_arr.push(element);
+    });
+    return ret_arr;
 };
 
 // Do not edit below this line
