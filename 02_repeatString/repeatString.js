a136ed4b651fb4434d7dc49f7ebd702f010f9386
Ajraa <salajkapavel@gmail.com>
2023-06-07 13:29:57 +0200

    Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    if(num < 0)
+        return 'ERROR';
+    let ret = '';
+    for (let i = 0; i < num; i++) {
+        ret += word;
+    }
+    return ret;
 };
 
 // Do not edit below this line
