fd4d946da6d44a4457b394bb67fd0c99dd725f7e
Ajraa <salajkapavel@gmail.com>
2023-06-07 15:12:57 +0200

    Exercises 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start < 0 || end <0 ||
+        !Number.isInteger(start) || !Number.isInteger(end))
+        return 'ERROR';
 
+    
+    if(start > end) {
+        const tmp = start;
+        start = end;
+        end = tmp;
+    }
+    sum = 0
+    for (let index = start; index <= end; index++) {
+        sum += index;
+    }
+    return sum;
 };
 
 // Do not edit below this line
